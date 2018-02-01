8f17bf94cfbabf96ce156e3286b51ab16ed4f633
Christoffer G. Thomsen <chris@cgt.name>
2018-02-01 17:04:56 +0100

    Oprydning
    Jeg havde skabt et testprojekt manuelt, men jeg slettede det med det
    samme og autogenererede et nyt testprojekt med samme navn. Visual Studio
    slettede ikke den SLETTEDE testprojektmappe og kaldte det testprojekt
    "Tests1". Typisk problem med buildsystemer baseret på metadatafiler i
    stedet for at udnytte filsystemet. Håbløst.

--- BilletLib.Tests/Properties/AssemblyInfo.cs
+++ BilletLib.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,36 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("BilletLib.Tests")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BilletLib.Tests1")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BilletLib.Tests")]
+[assembly: AssemblyProduct("BilletLib.Tests1")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: Guid("54e830c9-4ae7-40d5-9871-c58349531f7d")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("33faad05-cb8b-4d72-9619-3284ecdf9375")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
