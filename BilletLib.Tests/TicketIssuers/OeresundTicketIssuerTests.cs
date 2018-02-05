46be253d4d155dcf35db0450bd56e6b195cdab62
Christoffer G. Thomsen <chris@cgt.name>
2018-02-05 09:08:32 +0100

    Add tests for OeresundTicketIssuer
    TODO: more comprehensive test suite

---
+++ BilletLib.Tests/TicketIssuers/OeresundTicketIssuerTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib.TicketIssuers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.TicketIssuers.Tests
+{
+    [TestClass()]
+    public class OeresundTicketIssuerTests
+    {
+        [TestMethod()]
+        public void IssueTicketCarTest()
+        {
+            var issuer = new OeresundTicketIssuer();
+
+            var order = new TicketOrder
+            {
+                Timestamp = new DateTime(2018, 2, 2), // Friday
+                LicensePlate = "XX 12 345",
+                VehicleType = "car",
+                BroBizz = false,
+            };
+
+            var ticket = issuer.IssueTicket(order);
+
+            Assert.AreEqual(order.Timestamp, ticket.Timestamp);
+            Assert.AreEqual(order.LicensePlate, ticket.LicensePlate);
+            Assert.AreEqual(order.VehicleType, ticket.VehicleType);
+            Assert.AreEqual(410, ticket.Price);
+        }
+
+        [TestMethod()]
+        public void IssueTicketMotorcycleTest()
+        {
+            var issuer = new OeresundTicketIssuer();
+
+            var order = new TicketOrder
+            {
+                Timestamp = new DateTime(2018, 2, 2), // Friday
+                LicensePlate = "XX 12 345",
+                VehicleType = "motorcycle",
+                BroBizz = false,
+            };
+
+            var ticket = issuer.IssueTicket(order);
+
+            Assert.AreEqual(order.Timestamp, ticket.Timestamp);
+            Assert.AreEqual(order.LicensePlate, ticket.LicensePlate);
+            Assert.AreEqual(order.VehicleType, ticket.VehicleType);
+            Assert.AreEqual(210, ticket.Price);
+        }
+    }
+}
