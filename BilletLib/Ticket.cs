cbb504da8a2fc9d0d6d5ce3149195fe03f6442e5
Christoffer G. Thomsen <chris@cgt.name>
2018-03-06 14:34:21 +0100

    [DataContract] on models

--- BilletLib/Ticket.cs
+++ BilletLib/Ticket.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace BilletLib
 {
@@ -6,27 +7,32 @@ namespace BilletLib
     /// Ticket represents an issued ticket.
     /// </summary>
     [Serializable]
+    [DataContract]
     public class Ticket
     {
         /// <summary>
         /// The time when the ticket was ordered.
         /// </summary>
+        [DataMember]
         public DateTime Timestamp { get; }
 
         /// <summary>
         /// The license plate number of the vehicle the Ticket is for.
         /// </summary>
+        [DataMember]
         public string LicensePlate { get; }
 
         /// <summary>
         /// The type of vehicle the ticket is for.
         /// Allowable values are defined by the ITicketIssuer implementation.
         /// </summary>
+        [DataMember]
         public string VehicleType { get; }
 
         /// <summary>
         /// The final price that must be paid for the ticket.
         /// </summary>
+        [DataMember]
         public decimal Price { get; }
 
         public Ticket(TicketOrder o, decimal price)
--- BilletLib/TicketOrder.cs
+++ BilletLib/TicketOrder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace BilletLib
 {
@@ -6,27 +7,32 @@ namespace BilletLib
     /// TicketOrder represents an incoming request to issue a Ticket.
     /// </summary>
     [Serializable]
+    [DataContract]
     public class TicketOrder
     {
         /// <summary>
         /// The time when the ticket was ordered.
         /// </summary>
+        [DataMember]
         public DateTime Timestamp { get; set; }
 
         /// <summary>
         /// The license plate number of the vehicle the Ticket is for.
         /// </summary>
+        [DataMember]
         public string LicensePlate { get; set; }
 
         /// <summary>
         /// The type of vehicle the ticket is for.
         /// Allowable values are defined by the ITicketIssuer implementation.
         /// </summary>
+        [DataMember]
         public string VehicleType { get; set; }
 
         /// <summary>
         /// Whether or not the vehicle is equipped with a BroBizz device.
         /// </summary>
+        [DataMember]
         public bool BroBizz { get; set; }
     }
 }
